a3131743348bc6f5b398a87ceb665c31fd99ea60
Ian Young <ian@iay.org.uk>
2025-03-21 11:35:41 +0000

    Add signature validation to test

--- amazoncorretto-17/testing/test.sh
+++ amazoncorretto-17/testing/test.sh
@@ -9,8 +9,6 @@ SOFTHSM2_CONF=softhsm/softhsm2.conf \
     xmlsectool.sh --sign --inFile uk000006.xml --outFile uk000006-out.xml \
     --pkcs11Config pkcs11-softhsm.cfg --keyAlias key10 --keyPassword 12341234
 
-# echo Aggregate and Sign...
-# mda.sh aggregate-and-sign.xml main
-# xmlsectool.sh --verifySignature \
-#     --inFile path/to/output/aggregate-signed.xml \
-#     --certificate path/to/secrets/self-signed.pem
+xmlsectool.sh --verifySignature \
+     --inFile uk000006-out.xml \
+     --certificate secrets/self-signed.pem
